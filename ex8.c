d8f66d7bf7998155e8918816227d72dbf3b27cc4
Khadicha <246abdurashidova123@gmail.com>
2023-08-18 10:06:39 +0500

    ex8 done

---
+++ ex8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3, average;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    printf("Enter the third number: ");
+    scanf("%d", &num3);
+
+    average = (num1 + num2 + num3) / 3;
+
+    printf("The average of the three numbers is: %d\n", average);
+
+    return 0;
+}
