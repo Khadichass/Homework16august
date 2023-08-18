13f33624f6edad9ecd38d8503931880a4678350d
Khadicha <246abdurashidova123@gmail.com>
2023-08-18 09:29:00 +0500

    ex1 done

--- ex1.c
+++ ex1.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main() {
+
     char name[20];
     int age;
 
     printf("Enter your name: ");
     scanf("%s", name);
+
+    
     printf("Enter your age: ");
     scanf("%d", &age);
 
