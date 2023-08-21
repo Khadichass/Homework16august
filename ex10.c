fce6d97c20d0be4504ab13bfc99dab47645e5f86
Khadicha <246abdurashidova123@gmail.com>
2023-08-21 11:20:55 +0500

    some changes added

--- ex10.c
+++ ex10.c
@@ -8,10 +8,10 @@ int main(){
   scanf("%d",&year);
   if (year % 4 == 0 && year % 100 !=0)
   {
-    printf("%d year is leap year\n",year)
+    printf("%d year is leap year\n",year);
   }else
   {
-    printf("%d year is not leap year\n",year)
+    printf("%d year is not leap year\n",year);
   }
-  
+  return 0;
 }
